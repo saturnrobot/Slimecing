e57cffc304a322b3538e1590477bb1700e398930
PlutoTank <qwolkensperg@gmail.com>
2020-04-16 15:26:53 -0400

    Added two dimentional axis trigger

---
+++ Assets/Scripts/Slimecing/Triggers/TriggerLogic/TwoDimensionalAxisInputTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Slimecing.Triggers.TriggerLogic
+{
+    public class TwoDimensionalAxisInputTrigger : TriggerInput
+    {
+        protected override void TriggerStarted(GameObject player, InputAction.CallbackContext ctx)
+        {
+            currentTriggerState = TriggerState.Performed;
+            OnTriggerStateChange(new TriggerPackage(TriggerState.Performed, player, ctx));
+        }
+
+        protected override void TriggerPerformed(GameObject player, InputAction.CallbackContext ctx)
+        {
+            currentTriggerState = TriggerState.Performed;
+            OnTriggerStateChange(new TriggerPackage(TriggerState.Performed, player, ctx));
+        }
+
+        protected override void TriggerCanceled(GameObject player, InputAction.CallbackContext ctx)
+        {
+            currentTriggerState = TriggerState.Canceled;
+            OnTriggerStateChange(new TriggerPackage(TriggerState.Canceled, player, ctx));
+        }
+    }
+}
