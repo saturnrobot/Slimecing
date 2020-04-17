1c1e02e2972edd7f877d7ad4d1b6441aa99af0f2
PlutoTank <qwolkensperg@gmail.com>
2020-04-17 19:28:44 -0400

    github in unity does not add everything for some reason

---
+++ Assets/Scripts/Slimecing/Swords/Orbitals/OrbitalStats.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Slimecing.Swords.Orbitals
+{
+    [CreateAssetMenu(fileName = "OrbitalStats", menuName = "Swords/Orbitals/OrbitalStats")]
+    public class OrbitalStats : ScriptableObject
+    {
+        [SerializeField] private float orbitalSpeed;
+        [SerializeField] private float yOffset;
+        [SerializeField] private float xAxis = 5f;
+        [SerializeField] private float yAxis = 5f;
+
+        public float rotationSpeed
+        {
+            get => orbitalSpeed;
+            set => orbitalSpeed = value;
+        }
+
+        public float verticalOffset
+        {
+            get => yOffset;
+            set => yOffset = value;
+        }
+
+        public float radiusX
+        {
+            get => xAxis;
+            set => xAxis = value;
+        }
+
+        public float radiusY
+        {
+            get => yAxis;
+            set => yAxis = value;
+        }
+    }
+}
