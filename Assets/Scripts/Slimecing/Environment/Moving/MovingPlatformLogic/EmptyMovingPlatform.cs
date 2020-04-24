be867c3c7cd6256698ec5de62eeb88e902395049
PlutoTank <qwolkensperg@gmail.com>
2020-04-23 21:29:22 -0400

    More moving platform types

---
+++ Assets/Scripts/Slimecing/Environment/Moving/MovingPlatformLogic/EmptyMovingPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Slimecing.Environment.Moving.MovingPlatformLogic
+{
+    public class EmptyMovingPlatform : MovingController
+    {
+        public override void TickMover(out Vector3 targetPosition, out Quaternion targetRotation, float deltaTime)
+        {
+            var moverTransform = transform;
+            targetPosition = mover.moverRigidbody.position;
+            targetRotation = mover.moverRigidbody.rotation;
+        }
+    }
+}
